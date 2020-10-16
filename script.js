1f693b93f10255bff671be9c7855cdcea8ee165a
Parker Morgan <jpmaster13@msn.com>
2020-10-15 22:39:21 -0600

    Made the time change the class of each row, adjusted the names of classes to match premade styles

--- script.js
+++ script.js
@@ -6,30 +6,61 @@ $(document).ready(function() {
     //static on page load
     var times = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
     var date = moment().format('MMMM Do YYYY');
+
+    //needed global vars
+    var lastHour;
+    var currentHour = moment().format('HH');
     
     //initial dom populator
     todayEl.text(date);
     for(var i = 0; i<times.length; i++){
     
-    var basic = $("<div></div>");
-    var firstEl = $("<div class = 'col-1'>"+times[i]+"</div>");
-    var secondEl = $("<div class = 'col-10'></div>");
-    var thirdEl = $("<div class = 'col-1'></div>");
-    var inputEl = $("<textarea class='container'></textarea>")
-    var saveBtn = $("<button>Save</button>")
+      var basic = $("<div>");
+      basic.attr('class', 'time-block row');
+      var firstEl = $("<div>");
+      firstEl.attr('class', 'hour col-2').text(times[i]);
+      var secondEl = $("<textarea>");
+      secondEl.attr('class', 'description col-8');
+      var thirdEl = $("<button>");
+      thirdEl.attr('class', 'saveBtn col-2').text('save');
+      
     
-    basic.attr('class', 'row tBox' + i );
-    basic.append(firstEl);
-    basic.append(secondEl);
-    secondEl.append(inputEl);
-    thirdEl.append(saveBtn);
-    basic.append(thirdEl);
-    area.append(basic);
+      
+      basic.append(firstEl);
+      basic.append(secondEl);      
+      basic.append(thirdEl);
+      area.append(basic);
     }
+    updateTimeClasses();
 
+    setInterval(tick, 6000);
+    function tick(){
+      currentHour = moment().format('HH') - 6;
+      console.log(currentHour);
+      if(lastHour !== currentHour){
+        updateTimeClasses();
+      }
+      lastHour = currentHour;
+    }
     
-    
+    function updateTimeClasses(){
+      // hourindex needs to be current - 9
+      var hourIndex = currentHour - 9;
+      console.log(hourIndex)
+      $(".time-block").each(function(timeBox){
+        console.log(timeBox);
 
+        if(timeBox < hourIndex){
+          $(this).addClass('past').removeClass('present').removeClass('future');
+        }
+        if(timeBox === hourIndex){
+          $(this).addClass('present').removeClass('past').removeClass('future');
+        }
+        if(timeBox > hourIndex){
+          $(this).addClass('future').removeClass('present').removeClass('past');
+        }
+      })
+    }
 
 
 
